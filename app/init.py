1952870d31df01af02561123024e412a7dc91ddf
Akshay Lende <akshaylende48@gmail.com>
2026-01-30 00:51:33 +0530

    Adding DB and models for tables employee information

--- app/init.py
+++ app/init.py
@@ -1,3 +1,12 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+# creating an instance of flask app here 
 flask_app = Flask(__name__)
+
+# setting up database connections
+flask_app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hrms.db'
+flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# creating an instance of sqlite db 
+db = SQLAlchemy(flask_app)
---
+++ app/models.py
@@ -0,0 +1,15 @@
+from app.init import db
+
+class Employee(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    emp_id = db.Column(db.String(20), unique=True, nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    department = db.Column(db.String(50))
+
+
+class Attendance(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    employee_id = db.Column(db.Integer, db.ForeignKey('employee.id'), nullable=False)
+    date = db.Column(db.Date, nullable=False)
+    status = db.Column(db.String(10), nullable=False) # Present/Absent
