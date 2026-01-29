a61ceb44404e39fbd11ce1d328327e73b4467fcc
Akshay Lende <akshaylende48@gmail.com>
2026-01-30 00:54:01 +0530

    Handling operational routes for employee data and attendance tracking

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,19 @@
+from flask import request
 from app.init import flask_app
+from app.services import add_employee_logic, mark_attendance_logic
 
 @flask_app.route('/')
 def home():
     return ('Backend Server Started !')
+
+
+# Request Handling for Creating an employee entry in our DB 
+@flask_app.route('/api/employees', methods=['POST'])
+def add_employee():
+    return add_employee_logic(request.json)
+
+
+# Request Handling for marking attendence for an existing employee
+@flask_app.route('/api/attendance', methods=['POST'])
+def mark_attendance():
+    return mark_attendance_logic(request.json)
--- run.py
+++ run.py
@@ -1,6 +1,15 @@
-from app.init import flask_app
+from app.init import flask_app, db
+
+# Adding routes here to register 
+import app.routes
 
 
 if __name__ == "__main__":
+
+    # with flask_app context initializing db 
+    with flask_app.app_context():
+        db.create_all()
+
+    # debug = True for development purposes 
     flask_app.run(debug = True)
 
