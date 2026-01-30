ee116c06fd83a495829a896617616788872ec224
Akshay Lende <akshaylende48@gmail.com>
2026-01-30 15:15:05 +0530

    Update CORS for production

--- app/init.py
+++ app/init.py
@@ -6,7 +6,7 @@ from flask_cors import CORS
 flask_app = Flask(__name__)
 
 # Enabling CORS for the frontend port
-CORS(flask_app, resources={r"/api/*": {"origins": "http://localhost:1234"}})
+CORS(flask_app, resources={r"/api/*": {"origins": ['http://localhost:1234', 'https://hrms-frontend-p5em.onrender.com']}})
 
 # setting up database connections
 flask_app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hrms.db'
